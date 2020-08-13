012821e15545da7e6b9d847c52080cf74f03629b
chen jie <jie.chen@shui-inc.com>
2020-08-13 10:38:28 +0800

    package --update

--- src/index.tsx
+++ src/index.tsx
@@ -4,19 +4,10 @@ import { AppContainer } from 'react-hot-loader';
 import { BrowserRouter } from 'react-router-dom';
 import Router from './router';
 import $ from 'jquery';
-import { add } from './math.js';
+// import { add } from './math';
 
-add(66);
+// add(66);
 /*初始化*/
-renderWithHotReload(Router);
-
-/*热更新*/
-if (module.hot) {
-    module.hot.accept('./router/index.tsx', () => {
-        const Router = require('./router/index.tsx').default;
-        renderWithHotReload(Router);
-    });
-}
 function renderWithHotReload(Router) {
     ReactDOM.render(
         <AppContainer>
@@ -26,6 +17,16 @@ function renderWithHotReload(Router) {
         </AppContainer>,
         document.getElementById('app')
     );
+}
+
+renderWithHotReload(Router);
+
+/*热更新*/
+if ((module as any).hot) {
+    (module as any).hot.accept('./router/index.tsx', () => {
+        const Router = require('./router/index.tsx').default;
+        renderWithHotReload(Router);
+    });
 }
 
 // 判断该浏览器支不支持 serviceWorker
